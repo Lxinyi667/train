525d1d81bbdb9a0ae328d56d810431aa4f976b41
nameVal <eamil@qq.com>
2023-11-20 22:07:26 +0800

    增加Web 控台主页

--- web/src/views/login.vue
+++ web/src/views/login.vue
@@ -39,6 +39,9 @@
 import { reactive } from 'vue'
 import axios from 'axios'
 import { notification } from 'ant-design-vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
 const loginForm = reactive({
   mobile: '13000000000',
   code: ''
@@ -72,6 +75,8 @@ const login = () => {
     if (data.success) {
       notification.success({ description: '登录成功！' })
       console.log('登录成功：', data.content)
+      // 登录成功，跳到控台主页
+      router.push('/')
     } else {
       notification.error({ description: data.message })
     }

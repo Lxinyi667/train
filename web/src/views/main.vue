20aae0a70d4b83a0479011f403822a8f80e47bd5
nameVal <eamil@qq.com>
2023-11-20 22:14:59 +0800

    制作Vueå公共组件——the-header.vue组件

--- web/src/views/main.vue
+++ web/src/views/main.vue
@@ -1,6 +1,6 @@
 <template>
     <a-layout id="components-layout-demo-top-side-2">
-        <a-layout-header class="header">
+        <!-- <a-layout-header class="header">
             <div class="logo" />
             <a-menu
                 v-model:selectedKeys="selectedKeys1"
@@ -12,7 +12,8 @@
                 <a-menu-item key="2">nav 2</a-menu-item>
                 <a-menu-item key="3">nav 3</a-menu-item>
             </a-menu>
-        </a-layout-header>
+        </a-layout-header> -->
+        <TheHeaderView></TheHeaderView>
     <a-layout>
         <a-layout-sider width="200" style="background:#fff">
             <a-menu
@@ -87,7 +88,8 @@ import {
 }
   from '@ant-design/icons-vue'
 import { ref } from 'vue'
-const selectedKeys1 = ref(['2'])
+import TheHeaderView from '@/components/the-header.vue'
+
 const selectedKeys2 = ref(['1'])
 const openKeys = ref(['sub1'])
 </script>

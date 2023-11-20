5753e9fd35af9ca9083c4146aa02d6af0a7de37d
nameVal <eamil@qq.com>
2023-11-20 18:55:33 +0800

    集成校验框架Validation

--- member/src/main/java/top/lxyi/train/member/controller/MemberController.java
+++ member/src/main/java/top/lxyi/train/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package top.lxyi.train.member.controller;
 
 import jakarta.annotation.Resource;
+import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ public class MemberController {
 //    public long register(String mobile){
 //        return memberService.register(mobile);
 //    }
-    public CommonResp<Long> register(MemberRegisterReq req){
+    public CommonResp<Long> register(@Valid MemberRegisterReq req){
         long register = memberService.register(req);
         return new CommonResp<>(register);
     }
--- member/src/main/java/top/lxyi/train/member/req/MemberRegisterReq.java
+++ member/src/main/java/top/lxyi/train/member/req/MemberRegisterReq.java
@@ -1,5 +1,6 @@
 package top.lxyi.train.member.req;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -10,5 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class MemberRegisterReq {
+    @NotBlank(message = "[手机号]不能为空")
     private String mobile;
 }

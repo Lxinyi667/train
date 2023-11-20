1fc80611a4989c0f64a5f32fec2cea970e71a03a
nameVal <eamil@qq.com>
2023-11-20 19:47:49 +0800

    注册登录二合一界面开发

--- web/src/views/AboutView.vue
+++ web/src/views/AboutView.vue
@@ -2,6 +2,6 @@
   <div class="about">
     <h1>This is an about page</h1>
     <a-button type="primary">测试</a-button>
-    <plus-circle-outlind />
+    <plus-circle-outlined />
   </div>
 </template>

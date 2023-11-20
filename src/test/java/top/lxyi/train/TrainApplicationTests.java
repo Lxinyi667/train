9df37a290586ef6df1adbb1ca661fbd2dfd15b55
nameVal <eamil@qq.com>
2023-11-20 15:23:34 +0800

    项目初始化配置

---
+++ src/test/java/top/lxyi/train/TrainApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lxyi.train;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

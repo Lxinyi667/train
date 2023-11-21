68483114bf145a75300283ee36e7d6243f392327
nameVal <eamil@qq.com>
2023-11-21 18:35:09 +0800

    保存成功后刷新列表

--- web/src/views/main/passenger.vue
+++ web/src/views/main/passenger.vue
@@ -67,6 +67,10 @@ const handleOk = () => {
     if (data.success) {
       notification.success({ description: '保存成功！' })
       visible.value = false
+      handleQuery({
+        page: pagination.current,
+        size: pagination.pageSize
+      })
     } else {
       notification.error({ description: data.message })
     }

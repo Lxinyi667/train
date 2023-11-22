dd813ef60c10f951425c90795234adba70158521
nameVal <eamil@qq.com>
2023-11-22 16:02:39 +0800

    车次表单增加车站下拉选择 增加后端接口

--- business/src/main/java/top/lxyi/train/business/service/TrainService.java
+++ business/src/main/java/top/lxyi/train/business/service/TrainService.java
@@ -41,7 +41,12 @@ train.setUpdateTime(now);
 trainMapper.updateByPrimaryKey(train);
 }
 }
-
+    public List<TrainQueryResp> queryAll() {
+        TrainExample trainExample = new TrainExample();
+        trainExample.setOrderByClause("code desc");
+        List<Train> trainList = trainMapper.selectByExample(trainExample);
+        return BeanUtil.copyToList(trainList, TrainQueryResp.class);
+    }
 public PageResp<TrainQueryResp> queryList(TrainQueryReq req) {
     TrainExample trainExample = new TrainExample();
     trainExample.setOrderByClause("id desc");

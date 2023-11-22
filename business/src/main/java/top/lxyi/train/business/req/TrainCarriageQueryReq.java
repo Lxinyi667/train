05d7b27cb4ad076c65ecd70a70a8492dd015a475
nameVal <eamil@qq.com>
2023-11-22 13:33:46 +0800

    快速生成火车车厢管理功能

---
+++ business/src/main/java/top/lxyi/train/business/req/TrainCarriageQueryReq.java
@@ -0,0 +1,12 @@
+package top.lxyi.train.business.req;
+
+
+import top.lxyi.train.common.req.PageReq;
+
+public class TrainCarriageQueryReq extends PageReq {
+@Override
+public String toString() {
+return "TrainCarriageQueryReq{" +
+"} " + super.toString();
+}
+}

5428faf825245a8cc797844ccd0a132c3c025a97
nameVal <eamil@qq.com>
2023-11-22 14:01:29 +0800

    快速生成火车座位管理功能

--- admin/src/assets/js/enums.js
+++ admin/src/assets/js/enums.js
@@ -14,3 +14,15 @@ SEAT_TYPE_ARRAY = [
   { code: '3', desc: '软卧' },
   { code: '4', desc: '硬座' }
 ]
+
+SEAT_COL_ARRAY = [
+  { code: 'A', desc: 'A' },
+  { code: 'C', desc: 'C' },
+  { code: 'D', desc: 'D' },
+  { code: 'F', desc: 'F' },
+  { code: 'A', desc: 'A' },
+  { code: 'B', desc: 'B' },
+  { code: 'C', desc: 'C' },
+  { code: 'D', desc: 'D' },
+  { code: 'F', desc: 'F' }
+]
---
+++ business/src/main/java/top/lxyi/train/business/req/TrainSeatQueryReq.java
@@ -0,0 +1,12 @@
+package top.lxyi.train.business.req;
+
+
+import top.lxyi.train.common.req.PageReq;
+
+public class TrainSeatQueryReq extends PageReq {
+@Override
+public String toString() {
+return "TrainSeatQueryReq{" +
+"} " + super.toString();
+}
+}

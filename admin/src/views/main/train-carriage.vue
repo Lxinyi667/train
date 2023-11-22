cccff7768c2f1f934c495a9e693da649f20ac48d
nameVal <eamil@qq.com>
2023-11-22 17:28:58 +0800

    火车座位页面的è½车厢¬¡改为车次选择组件

--- admin/src/views/main/train-carriage.vue
+++ admin/src/views/main/train-carriage.vue
@@ -34,8 +34,8 @@
         <a-modal v-model:visible="visible" title="火车车厢" @ok="handleOk"
                  ok-text="确认" cancel-text="取消">
             <a-form :model="trainCarriage" :label-col="{span: 4}" :wrapper-col="{ span: 20 }">
-                        <a-form-item label="车次编号">
-                                <a-input v-model:value="trainCarriage.trainCode"/>
+                       <a-form-item label="车次编号">
+                          <train-select-view v-model="trainCarriage.trainCode"></train-select-view>
                         </a-form-item>
                         <a-form-item label="厢号">
                                 <a-input v-model:value="trainCarriage.index"/>
@@ -65,7 +65,7 @@
 import { ref, onMounted } from 'vue'
 import { notification } from 'ant-design-vue'
 import axios from 'axios'
-
+import TrainSelectView from '@/components/train-select.vue'
 const SEAT_TYPE_ARRAY = window.SEAT_TYPE_ARRAY
 const visible = ref(false)
 const trainCarriage = ref({

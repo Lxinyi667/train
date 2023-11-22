48af2051f39aaf34032f3d69b8c2d4a8d038b09a
nameVal <eamil@qq.com>
2023-11-22 19:51:31 +0800

    完善——为途经车站自动计算停站时长

--- admin/src/views/main/train-station.vue
+++ admin/src/views/main/train-station.vue
@@ -53,8 +53,11 @@
                                                    placeholder="请选择时间"/>
                         </a-form-item>
                         <a-form-item label="停站时长">
-                                    <a-time-picker v-model:value="trainStation.stopTime" valueFormat="HH:mm:ss"
-                                                   placeholder="请选择时间"/>
+                                    <a-time-picker
+                                    v-model:value="trainStation.stopTime"
+                                    valueFormat="HH:mm:ss"
+                                    placeholder="请选择时间"
+                                    disabled />
                         </a-form-item>
                         <a-form-item label="里程（公里）">
                                 <a-input v-model:value="trainStation.km"/>
@@ -70,6 +73,7 @@ import axios from 'axios'
 import { pinyin } from 'pinyin-pro'
 import TrainSelectView from '@/components/train-select.vue'
 import StationSelectView from '@/components/station-select.vue'
+import dayjs from 'dayjs'
 
 const visible = ref(false)
 const trainStation = ref({
@@ -154,6 +158,36 @@ watch(
     } else {
       trainStation.value.namePinyin = ''
     }
+  },
+  { immediate: true }
+)
+// 完善
+// 自动计算停车时长
+watch(
+  () => trainStation.value.inTime,
+  () => {
+    const diff = dayjs(trainStation.value.outTime, 'HH:mm:ss').diff(
+      dayjs(trainStation.value.inTime, 'HH:mm:ss'),
+      'seconds'
+    )
+    trainStation.value.stopTime = dayjs('00:00:00', 'HH:mm:ss')
+      .second(diff)
+      .format('HH:mm:ss')
+  },
+  { immediate: true }
+)
+
+// 自动计算停车时长
+watch(
+  () => trainStation.value.outTime,
+  () => {
+    const diff = dayjs(trainStation.value.outTime, 'HH:mm:ss').diff(
+      dayjs(trainStation.value.inTime, 'HH:mm:ss'),
+      'seconds'
+    )
+    trainStation.value.stopTime = dayjs('00:00:00', 'HH:mm:ss')
+      .second(diff)
+      .format('HH:mm:ss')
   },
   { immediate: true }
 )

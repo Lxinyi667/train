543a7161a83b64202ae3004f9f7b8702e38f2fc3
nameVal <eamil@qq.com>
2023-11-22 19:26:09 +0800

    完善——保存车站之前，先校验唯一键是否存在

--- business/src/main/java/top/lxyi/train/business/service/StationService.java
+++ business/src/main/java/top/lxyi/train/business/service/StationService.java
@@ -1,10 +1,13 @@
 package top.lxyi.train.business.service;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.ObjectUtil;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import top.lxyi.train.common.exception.BusinessException;
+import top.lxyi.train.common.exception.BusinessExceptionEnum;
 import top.lxyi.train.common.resp.PageResp;
 import top.lxyi.train.common.util.SnowUtil;
 import top.lxyi.train.business.domain.Station;
@@ -32,6 +35,14 @@ public void save(StationSaveReq req) {
     DateTime now = DateTime.now();
     Station station = BeanUtil.copyProperties(req, Station.class);
     if (ObjectUtil.isNull(station.getId())) {
+        //保存之前，先校验一键是否存在
+        StationExample stationExample = new StationExample();
+        stationExample.createCriteria().andNameEqualTo(req.getName());
+        List<Station> list = stationMapper.selectByExample(stationExample);
+        if (CollUtil.isNotEmpty(list)){
+            throw new BusinessException(BusinessExceptionEnum.BUSINESS_STATION_NAME_UNIQUE_ERROR);
+        }
+
         station.setId(SnowUtil.getSnowflakeNextId());
         station.setCreateTime(now);
         station.setUpdateTime(now);

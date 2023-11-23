1b697452f7b1176e0564d2c1ec09a6e78acc14f5
nameVal <eamil@qq.com>
2023-11-23 22:41:36 +0800

    多节点场景中 quartz 的调度情况

--- batch/src/main/java/top/lxyi/train/batch/job/Test3Job.java
+++ batch/src/main/java/top/lxyi/train/batch/job/Test3Job.java
@@ -6,16 +6,11 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 @DisallowConcurrentExecution
-public class TestJob implements Job {
+public class Test3Job implements Job {
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
-        System.out.println("TestJob TEST 开始");
-        //try {
-        //    Thread.sleep(3000);
-        //} catch (InterruptedException e) {
-        //    e.printStackTrace();
-        //}
-        System.out.println("TestJob TEST 结束");
+        System.out.println("TestJob333333333 TEST开始");
+        System.out.println("TestJob333333333 TEST结束");
     }
 }

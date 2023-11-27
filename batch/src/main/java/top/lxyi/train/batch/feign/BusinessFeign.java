8f781f6dd83f4cdd29aa3ba15b4b190de3bfee4a
nameVal <eamil@qq.com>
2023-11-27 16:53:24 +0800

    配置 Feign + Nacos 支持应用名远程调用

--- batch/src/main/java/top/lxyi/train/batch/feign/BusinessFeign.java
+++ batch/src/main/java/top/lxyi/train/batch/feign/BusinessFeign.java
@@ -7,11 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import top.lxyi.train.common.resp.CommonResp;
 
 import java.util.Date;
-
-@FeignClient(name = "business",url = "http://127.0.0.1:8002/business")
+@FeignClient(name = "business")
+//@FeignClient(name = "business",url = "http://127.0.0.1:8002/business")
 public interface BusinessFeign {
     @GetMapping("/hello")
-    String hello();
+    String getHello();
 
     @GetMapping("/admin/daily-train/gen-daily/{date}")
     CommonResp<Object> genDaily(@PathVariable @DateTimeFormat(pattern ="yyyy-MM-dd") Date date);
--- business/src/main/java/top/lxyi/train/business/feign/MemberFeign.java
+++ business/src/main/java/top/lxyi/train/business/feign/MemberFeign.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import top.lxyi.train.common.req.MemberTicketReq;
 import top.lxyi.train.common.resp.CommonResp;
-
-@FeignClient(name = "member", url = "http://127.0.0.1:8001")
+@FeignClient(name = "member")
+//@FeignClient(name = "member", url = "http://127.0.0.1:8001")
 public interface MemberFeign {
 
     @GetMapping("/member/feign/ticket/save")

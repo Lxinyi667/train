9acb8a7c25a38184c408cad753cf883981148bdc
nameVal <eamil@qq.com>
2023-11-27 10:00:43 +0800

    美化车次信息的显示

--- web/src/views/main/order.vue
+++ web/src/views/main/order.vue
@@ -1,8 +1,22 @@
 <template>
-<div>{{ dailyTrainTicket }}</div>
+<div>
+    <span class="order-train-main">{{dailyTrainTicket.date }}</span>&nbsp;
+    <span class="order-train-main">{{dailyTrainTicket.trainCode }}</span>&nbsp;
+    <span class="order-train-main">{{dailyTrainTicket.start }}</span>
+    <span class="order-train-main">({{dailyTrainTicket.startTime }})</span>&nbsp;
+    <span class="order-train-main">——</span>&nbsp;
+    <span class="order-train-main">{{dailyTrainTicket.end }}</span>
+    <span class="order-train-main">({{dailyTrainTicket.endTime }})</span>&nbsp;
+</div>
 </template>
 
 <script setup>
 const dailyTrainTicket = SessionStorage.get(SESSION_ORDER) || {}
-console.Log('下单的车次信息', dailyTrainTicket)
+console.log('下单的车次信息', dailyTrainTicket)
 </script>
+<style scoped>
+.order-train .order-train-main{
+    font-size:18px;
+    font-weight:bold;
+}
+</style>

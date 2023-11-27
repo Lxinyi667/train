3326c13cb1f6545987a682e1fbfd869672a03ff7
nameVal <eamil@qq.com>
2023-11-27 15:29:06 +0800

    计算相对第一个座位的偏移值

--- business/src/main/java/top/lxyi/train/business/service/ConfirmOrderService.java
+++ business/src/main/java/top/lxyi/train/business/service/ConfirmOrderService.java
@@ -4,11 +4,13 @@ import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.util.EnumUtil;
 import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson.JSON;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import top.lxyi.train.business.domain.DailyTrainTicket;
 import top.lxyi.train.business.enums.ConfirmOrderStatusEnum;
+import top.lxyi.train.business.enums.SeatColEnum;
 import top.lxyi.train.business.enums.SeatTypeEnum;
 import top.lxyi.train.business.req.ConfirmOrderTicketReq;
 import top.lxyi.train.common.context.LoginMemberContext;
@@ -27,6 +29,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -53,13 +56,15 @@ public void save(ConfirmOrderDoReq req) {
     }
 }
 public void doConfirm(ConfirmOrderDoReq req){
-//省略业务数据校验，如：车次是否存在，余票是否存在，车次是否在有效期内，t1ckts条数>0，同乘客同车次是否已买过
+    // 省略业务数据校验，如：车次是否存在，余票是否存在，车次是否在有效期内，tickets条数>0，同乘客同车次是否已买过
+
     Date date = req.getDate();
     String trainCode = req.getTrainCode();
     String start = req.getStart();
     String end = req.getEnd();
-//保存确认订单表，状态初始
-    DateTime now =DateTime.now();
+    List<ConfirmOrderTicketReq> tickets = req.getTickets();
+    // 保存确认订单表，状态初始
+    DateTime now = DateTime.now();
     ConfirmOrder confirmOrder = new ConfirmOrder();
     confirmOrder.setId(SnowUtil.getSnowflakeNextId());
     confirmOrder.setCreateTime(now);
@@ -71,16 +76,54 @@ public void doConfirm(ConfirmOrderDoReq req){
     confirmOrder.setEnd(end);
     confirmOrder.setDailyTrainTicketId(req.getDailyTrainTicketId());
     confirmOrder.setStatus(ConfirmOrderStatusEnum.INIT.getCode());
-    confirmOrder.setTickets(JSON.toJSONString(req.getTickets()));
+    confirmOrder.setTickets(JSON.toJSONString(tickets));
     confirmOrderMapper.insert(confirmOrder);
 
-    //查出余票记录，需要得到真实的库存
-    DailyTrainTicket dailyTrainTicket = dailyTrainTicketService.selectByUnique(date,trainCode,start,end);
-
+    // 查出余票记录，需要得到真实的库存
+    DailyTrainTicket dailyTrainTicket = dailyTrainTicketService.selectByUnique(date, trainCode, start, end);
     LOG.info("查出余票记录：{}", dailyTrainTicket);
-    //扣减余票数量，并判断余票是否足够
-    reduceTickets(req,dailyTrainTicket);
+
+    // 扣减余票数量，并判断余票是否足够
+    reduceTickets(req, dailyTrainTicket);
     LOG.info("扣减余票数量：{}", dailyTrainTicket);
+
+    // 计算相对第一个座位的偏移值
+    // 比如选择的是C1,D2，则偏移值是：[0,5]
+    // 比如选择的是A1,B1,C1，则偏移值是：[0,1,2]
+    ConfirmOrderTicketReq ticketReq0 = tickets.get(0);
+    if (StrUtil.isNotBlank(ticketReq0.getSeat())) {
+        LOG.info("本次购票有选座");
+        // 查出本次选座的座位类型都有哪些列，用于计算所选座位与第一个座位的偏离值
+        List<SeatColEnum> colEnumList = SeatColEnum.getColsByType(ticketReq0.getSeatTypeCode());
+        LOG.info("本次选座的座位类型包含的列：{}", colEnumList);
+
+        // 组成和前端两排选座一样的列表，用于作参照的座位列表，例：referSeatList = {A1, C1, D1, F1, A2, C2, D2, F2}
+        List<String> referSeatList = new ArrayList<>();
+        for (int i = 1; i <= 2; i++) {
+            for (SeatColEnum seatColEnum : colEnumList) {
+                referSeatList.add(seatColEnum.getCode() + i);
+            }
+        }
+        LOG.info("用于作参照的两排座位：{}", referSeatList);
+
+        List<Integer> offsetList = new ArrayList<>();
+        // 绝对偏移值，即：在参照座位列表中的位置
+        List<Integer> aboluteOffsetList = new ArrayList<>();
+        for (ConfirmOrderTicketReq ticketReq : tickets) {
+            int index = referSeatList.indexOf(ticketReq.getSeat());
+            aboluteOffsetList.add(index);
+        }
+        LOG.info("计算得到所有座位的绝对偏移值：{}", aboluteOffsetList);
+        for (Integer index : aboluteOffsetList) {
+            int offset = index - aboluteOffsetList.get(0);
+            offsetList.add(offset);
+        }
+        LOG.info("计算得到所有座位的相对第一个座位的偏移值：{}", offsetList);
+
+    } else {
+        LOG.info("本次购票没有选座");
+    }
+
     //选座
     //一个车箱一个车箱的获取座位数据
     //挑选符合条件的座位，如果这个车箱不满足，则进入下个车箱（多个选座应该在同一个车厢)
@@ -130,29 +173,29 @@ public void doConfirm(ConfirmOrderDoReq req){
     }
 
     public PageResp<ConfirmOrderQueryResp> queryList(ConfirmOrderQueryReq req) {
-    ConfirmOrderExample confirmOrderExample = new ConfirmOrderExample();
-    confirmOrderExample.setOrderByClause("id desc");
-    ConfirmOrderExample.Criteria criteria = confirmOrderExample.createCriteria();
+        ConfirmOrderExample confirmOrderExample = new ConfirmOrderExample();
+        confirmOrderExample.setOrderByClause("id desc");
+        ConfirmOrderExample.Criteria criteria = confirmOrderExample.createCriteria();
 
-    LOG.info("查询页码：{}", req.getPage());
-    LOG.info("每页条数：{}", req.getSize());
-    PageHelper.startPage(req.getPage(), req.getSize());
-    List<ConfirmOrder> confirmOrderList = confirmOrderMapper.selectByExample(confirmOrderExample);
+        LOG.info("查询页码：{}", req.getPage());
+        LOG.info("每页条数：{}", req.getSize());
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<ConfirmOrder> confirmOrderList = confirmOrderMapper.selectByExample(confirmOrderExample);
 
-    PageInfo<ConfirmOrder> pageInfo = new PageInfo<>(confirmOrderList);
-    LOG.info("总行数：{}", pageInfo.getTotal());
-    LOG.info("总页数：{}", pageInfo.getPages());
+        PageInfo<ConfirmOrder> pageInfo = new PageInfo<>(confirmOrderList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
-    List<ConfirmOrderQueryResp> list = BeanUtil.copyToList(confirmOrderList, ConfirmOrderQueryResp.class);
+        List<ConfirmOrderQueryResp> list = BeanUtil.copyToList(confirmOrderList, ConfirmOrderQueryResp.class);
 
         PageResp<ConfirmOrderQueryResp> pageResp = new PageResp<>();
             pageResp.setTotal(pageInfo.getTotal());
             pageResp.setList(list);
             return pageResp;
-            }
+    }
 
-            public void delete(Long id) {
+    public void delete(Long id) {
             confirmOrderMapper.deleteByPrimaryKey(id);
             }
 
-            }
+}

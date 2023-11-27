f7509d8bd1ea44461b7e9c34b86f3c56f3c6d06d
nameVal <eamil@qq.com>
2023-11-27 16:19:43 +0800

    购票成功后，通过 feign 调用 member 的保存车票接口

---
+++ business/src/main/java/top/lxyi/train/business/feign/MemberFeign.java
@@ -0,0 +1,15 @@
+package top.lxyi.train.business.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import top.lxyi.train.common.req.MemberTicketReq;
+import top.lxyi.train.common.resp.CommonResp;
+
+@FeignClient(name = "member", url = "http://127.0.0.1:8001")
+public interface MemberFeign {
+
+    @GetMapping("/member/feign/ticket/save")
+    CommonResp<Object> save(@RequestBody MemberTicketReq req);
+
+}

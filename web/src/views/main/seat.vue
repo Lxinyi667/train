1e0b64e3b90bc2097d3abff40ec3d6666ce37c15
nameVal <eamil@qq.com>
2023-12-01 16:53:48 +0800

    座位销售图页面获得销售信息

--- web/src/views/main/seat.vue
+++ web/src/views/main/seat.vue
@@ -7,17 +7,47 @@
   </div>
   <div v-else>
     <p>
-      日期：{{ param.date }}，车次：{{ param.trainCode }}，出发站：{{ param.start }}，到达站：{{ param.end }}
+      日期：{{param.date}}，车次：{{param.trainCode}}，出发站：{{param.start}}，到达站：{{param.end}}
+    </p>
+    <p>
+      {{list}}
     </p>
   </div>
 </template>
-
 <script setup>
 
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import axios from 'axios'
+import { notification } from 'ant-design-vue'
 
 const route = useRoute()
 const param = ref({})
 param.value = route.query
+
+const list = ref()
+
+// 查询一列火车的所有销售信息
+const querySeat = () => {
+  axios.get('/business/seat-sell/query', {
+    params: {
+      date: param.value.date,
+      trainCode: param.value.trainCode
+    }
+  }).then((response) => {
+    const data = response.data
+    if (data.success) {
+      list.value = data.content
+    } else {
+      notification.error({ description: data.message })
+    }
+  })
+}
+
+onMounted(() => {
+  if (param.value.date) {
+    querySeat()
+  }
+})
+
 </script>

30ab47e144e51e778cd87bce2a3d111c5c7b93ba
nameVal <eamil@qq.com>
2023-12-01 08:11:34 +0800

    周五前最后样式

--- business/src/main/java/top/lxyi/train/business/controller/TestController.java
+++ business/src/main/java/top/lxyi/train/business/controller/TestController.java
@@ -1,12 +1,15 @@
 package top.lxyi.train.business.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @SentinelResource("hello")
     @GetMapping("/hello")
     public String hello(){
+
         return "Hello World ! Business!";
     }
 }
